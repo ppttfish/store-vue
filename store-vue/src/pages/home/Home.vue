b1e8ac78279a3a8e31d12352e07f0e9737282bde
ppttfish <ppttfish@163.com>
2018-07-18 20:20:51 +0800

    header init

---
+++ store-vue/src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+  </div>
+</template>
+
+<script>
+import NavHeader from './components/Header'
+export default {
+  name: 'home',
+  components: {
+    NavHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
