c9621af2dbc6b4b7356c8dcdb37686f633264f46
ppttfish <ppttfish@163.com>
2018-07-20 23:54:28 +0800

    header finished

--- store-vue/src/pages/home/components/Header.vue
+++ store-vue/src/pages/home/components/Header.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>header</div>
+  <div class="navbar">
+    <router-link to="/" class="navbar-left-container" tag="div">
+      <img src="../../../assets/pic/logo.png" alt="">
+    </router-link>
+    <div class="navbar-right-container">
+      <router-link to="/" class="navbar-login-link nav-link">login</router-link>
+      <router-link to="/" class="navbar-cart-link nav-link">
+        <svg class="icon cart-link" aria-hidden="true">
+          <use xlink:href="#icon-gouwuche"></use>
+        </svg>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +20,30 @@ export default {
 }
 </script>
 
-<style style="stylus" scoped>
-
+<style lang="stylus" scoped>
+  .navbar
+    display flex
+    justify-content space-between
+    align-items center
+    height 70px
+    padding 0 20px
+    .navbar-left-container
+      display block
+      width 50px
+      height 50px
+      img
+        width 100%
+    .navbar-right-container
+      display flex
+      align-items center
+      .nav-link
+        padding-left 15px
+      .navbar-login-link
+        color #666
+        font-size 16px
+      .navbar-cart-link
+        .cart-link
+          width 25px
+          height 25px
+          color black
 </style>
