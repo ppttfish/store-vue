1f669c36d3bd9bdf6dfe841871b4f41a74f58b0a
ppttfish <ppttfish@163.com>
2018-07-30 22:21:32 +0800

    navbread

--- store-vue/src/pages/home/Home.vue
+++ store-vue/src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <nav-header></nav-header>
+    <nav-bread></nav-bread>
   </div>
 </template>
 
 <script>
 import NavHeader from './components/Header'
+import NavBread from './components/NavBread'
 export default {
   name: 'home',
   components: {
-    NavHeader
+    NavHeader,
+    NavBread
   }
 }
 </script>
---
+++ store-vue/src/pages/home/components/NavBread.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="nav-bread">
+    <router-link to="/">Home</router-link>
+    <span>Goods</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navBread'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .nav-bread
+    padding 10px
+    background-color #f0f0f0
+    a
+      margin-right 10px
+      color #999
+      &::after
+        content '/'
+        position relative
+        left 5px
+</style>
